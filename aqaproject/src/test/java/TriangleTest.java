1398f3c1709d3e33b5ab3fc55194e5ad2c5c11bf
DianaHrymaliuk <diana.hrymaliuk@gmail.com>
2022-04-02 22:29:58 +0300

    1

--- aqaproject/src/test/java/TriangleTest.java
+++ aqaproject/src/test/java/TriangleTest.java
@@ -4,38 +4,38 @@ import org.junit.jupiter.api.Test;
 
 
 
-public class TriangleTest {
-
-    Triangle triangle = new Triangle(11, 12, 10);
-    Triangle triangle2 = new Triangle(-10, 10, 13);
-    Triangle triangle3 = new Triangle(13, 14, 12);
-
-    @Test
-    public void sum() {
-        int s = (int) (Triangle.sqr(triangle) + Triangle.sqr(triangle3));
-        int exp = 123;
-        Assertions.assertEquals(exp, s);
-    }
-
-    @Test
-    public void sqr1() {
-        Assertions.assertTrue(Triangle.sqr(triangle2) > 0);
-    }
-
-    @Test
-    public void sqr2() {
-        Assertions.assertTrue(Triangle.sqr(triangle) > 0);
-    }
-
-    @Test
-    public void not_null() {
-        Assertions.assertNotNull(Triangle.sqr(triangle));
-        Assertions.assertNotNull(Triangle.sqr(triangle3));
-    }
-
-    @Test
-    public void tr_equals() {
-        Assertions.assertEquals(Triangle.sqr(triangle), Triangle.sqr(triangle3));
-    }
-
-}
+//public class TriangleTest {
+//
+//    Triangle triangle = new Triangle(11, 12, 10);
+//    Triangle triangle2 = new Triangle(-10, 10, 13);
+//    Triangle triangle3 = new Triangle(13, 14, 12);
+//
+//    @Test
+//    public void sum() {
+//        int s = (int) (Triangle.sqr(triangle) + Triangle.sqr(triangle3));
+//        int exp = 123;
+//        Assertions.assertEquals(exp, s);
+//    }
+//
+//    @Test
+//    public void sqr1() {
+//        Assertions.assertTrue(Triangle.sqr(triangle2) > 0);
+//    }
+//
+//    @Test
+//    public void sqr2() {
+//        Assertions.assertTrue(Triangle.sqr(triangle) > 0);
+//    }
+//
+//    @Test
+//    public void not_null() {
+//        Assertions.assertNotNull(Triangle.sqr(triangle));
+//        Assertions.assertNotNull(Triangle.sqr(triangle3));
+//    }
+//
+//    @Test
+//    public void tr_equals() {
+//        Assertions.assertEquals(Triangle.sqr(triangle), Triangle.sqr(triangle3));
+//    }
+//
+//}
---
+++ aqaproject/src/test/java/andersenlab/TestUI.java
@@ -0,0 +1,54 @@
+package andersenlab;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class TestUI {
+    ChromeDriver driver;
+
+
+    @BeforeEach
+    public void before() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://andersenlab.com/");
+    }
+
+    @Test
+    public void background_color() {
+        //driver.findElement(By.xpath("//a[@class='MainNav-module--link--HnRAu MainNav-module--indexPage--RWmzP']")).click();
+        WebElement a = driver.findElement(By.xpath("//section[@id='experts']/div/div/div[5]"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView();", a);
+        driver.findElement(By.xpath("//section[@id='experts']/div/div/div[5]")).click();
+        String color = driver.findElement(By.xpath("//section[@id='experts']/div/div/div[5]")).getCssValue("background-color");
+        System.out.println(color);
+        Assertions.assertEquals("rgba(255, 219, 0, 1)", color);
+        driver.close();
+    }
+
+    @Test
+    public void skype_icon() {
+        WebElement b = driver.findElement(By.xpath("//a[normalize-space()='skype']"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView();", b);
+        driver.findElement(By.xpath("//a[normalize-space()='skype']")).click();
+        List<String> windowHandles = new ArrayList(driver.getWindowHandles());
+        String secondTab = windowHandles.get(1);
+        driver.switchTo().window(secondTab);
+        String c = driver.getCurrentUrl();
+        Assertions.assertEquals("https://join.skype.com/aM8R4P4dNJdy", c);
+        driver.close();
+    }
+}
