623cd92e9bb1d9c5be1ee51668425e81f4f9edfd
DianaHrymaliuk <diana.hrymaliuk@gmail.com>
2022-04-08 00:37:37 +0300

    1

--- aqaproject/src/main/java/lesson7/Les7.java
+++ aqaproject/src/main/java/lesson7/Les7.java
@@ -10,7 +10,7 @@ public class Les7 {
     public static void main(String[] args) throws IOException {
         String[] header = "Value 1; Value 2; Value 3".split(";");
         int[][] data = {{2, 6, 4}, {1, 3, 4}};
-        try (Writer writer = new PrintWriter("newfile.csv")) {
+        try (Writer writer = new PrintWriter("newfile1.csv")) {
             String sb = Arrays.toString(header) +
                     ";" +
                     Arrays.deepToString(data);
@@ -25,7 +25,7 @@ public class Les7 {
 
     public void save() {
         List<List<String>> record = new ArrayList<>();
-        try (Scanner scanner = new Scanner(new File("newfile.csv"))) {
+        try (Scanner scanner = new Scanner(new File("newfile1.csv"))) {
             while (scanner.hasNextLine()) {
                 record.add(line(scanner.nextLine()));
             }
