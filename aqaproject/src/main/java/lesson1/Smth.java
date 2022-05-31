183195c228a4811879e90275039d1908cac149a4
DianaHrymaliuk <diana.hrymaliuk@gmail.com>
2022-05-31 16:24:36 +0300

    1

---
+++ aqaproject/src/main/java/lesson1/Smth.java
@@ -0,0 +1,75 @@
+package lesson1;
+
+import java.util.Arrays;
+
+public class Smth {
+    public static void main(String[] args) {
+        str1(3, 2);
+        str1("Hello!");
+        for (int i = 0; i < 4; i++) {
+            str1("Smth");
+        }
+
+        int arr1 [] = {4,2,5,1,10};
+        String str2 [] = {"n", "a", "c", "b"};
+        seredynaMasyvu(arr1);
+        maxMinAvg(arr1);
+
+        Arrays.sort(arr1);
+        System.out.println("Метод сорт " + Arrays.toString(arr1));
+
+        bubbleSort(arr1);
+        System.out.println(arr1[0]);
+        System.out.println(arr1[arr1.length-1]);
+        Arrays.sort(str2);
+        System.out.println(Arrays.toString(str2));
+    }
+
+    public static void str1(int a, int b) {
+        int c;
+        c = a + b;
+        System.out.println(c);
+    }
+
+    public static void str1(String str) {
+        System.out.println(str);
+    }
+    public static void bubbleSort(int [] arr){
+        for (int i= arr.length-1; i>0; i--){
+            for (int j =0; j<i;j++){
+                if (arr[j]>arr[j+1]){
+                    int tmp = arr [j];
+                    arr[j] = arr[j+1];
+                    arr[j+1]=tmp;
+                }
+            }
+        }
+        System.out.println("Метод бульбашки " + Arrays.toString(arr));
+    }
+    public static void seredynaMasyvu(int [] arr){
+        int a = arr.length / 2;
+        System.out.println(arr[a]);
+    }
+    public static void maxMinAvg(int [] arr){
+        int max = arr[0];
+        int min = arr[0];
+        int sum = 0;
+        for (int i=0; i<arr.length; i++){
+            if(max < arr[i])
+                max = arr[i];
+            if(min > arr[i])
+                min = arr[i];
+        }
+        for ( int i = 0; i<arr.length; i++){
+            sum += arr[i];
+
+        }
+        int avg = sum/arr.length;
+        System.out.println("max: "+ max);
+        System.out.println("min: "+ min);
+        System.out.println("avg: "+ avg);
+        System.out.println("sum: "+ sum);
+    }
+}
+
+
